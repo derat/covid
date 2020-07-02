164e49b1b4c9c0b7b05c19b350b834ef91a8ed10
Daniel Erat <dan@erat.org>
2020-07-01 22:52:36 -0400

    Plot data using gnuplot.

--- mortality.go
+++ mortality.go
@@ -1,3 +1,6 @@
+// Copyright 2020 Daniel Erat <dan@erat.org>.
+// All rights reserved.
+
 package main
 
 import (
@@ -5,59 +8,166 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
-	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
 )
 
+const dateLayout = "20060102"
+
 func main() {
+	now := time.Now()
+
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %v [flags] <YYYYMMDD.csv> ...\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 	state := flag.String("state", "California", `State (as it appears in CSV files)`)
-	weekEnd := flag.String("week-end", "20200425", `Week end as "YYYYMMDD"`)
+	start := flag.String("start", now.AddDate(0, -3, 0).Format(dateLayout), `Starting week-end date`)
+	end := flag.String("end", now.Format(dateLayout), `Endiing week-end date`)
 	flag.Parse()
 
-	if y, m, d := parseDate(*weekEnd); y == "" || m == "" || d == "" {
-		log.Fatalf("Bad week end date %q; should be YYYYMMDD", *weekEnd)
+	startDate, err := time.Parse(dateLayout, *start)
+	if err != nil {
+		log.Fatalf("Bad -start date %q: %v", *start, err)
+	}
+	endDate, err := time.Parse(dateLayout, *end)
+	if err != nil {
+		log.Fatalf("Bad -end date %q: %v", *end, err)
 	}
 
+	// Read the CSV files.
+	ds := newDataSet(*state, startDate, endDate)
 	for _, p := range flag.Args() {
-		if err := readFile(p, *state, *weekEnd); err != nil {
+		if err := ds.readFile(p); err != nil {
 			log.Fatalf("Failed reading %v: %v", p, err)
 		}
 	}
+
+	// Write the data to a temp file.
+	dp, err := writeData(ds)
+	if err != nil {
+		log.Fatal("Failed writing data file: ", err)
+	}
+	defer os.Remove(dp)
+
+	// Write the gnuplot commands to a temp file.
+	gp, err := writeGnuplot(ds, dp)
+	if err != nil {
+		log.Fatal("Failed writing gnuplot file: ", err)
+	}
+	defer os.Remove(gp)
+
+	if err := exec.Command("gnuplot", "-p", gp).Run(); err != nil {
+		log.Fatal("Failed running gnuplot: ", err)
+	}
 }
 
-var dateRegexp = regexp.MustCompile(`^(20\d\d)([01]\d)([0-3]\d)$`)
-
-func parseDate(s string) (year, month, day string) {
-	m := dateRegexp.FindStringSubmatch(s)
-	if len(m) == 0 {
-		return "", "", ""
+// writeData creates a temp file and writes ds's data to it.
+// The file's path is returned.
+func writeData(ds *dataSet) (string, error) {
+	f, err := ioutil.TempFile("", "mortality.data.")
+	if err != nil {
+		return "", err
 	}
-	return m[1], m[2], m[3]
+	if err := ds.write(f); err != nil {
+		f.Close()
+		os.Remove(f.Name())
+		return "", err
+	}
+	err = f.Close()
+	return f.Name(), err
 }
 
-func readFile(p, state, weekEnd string) error {
-	base := filepath.Base(p)
-	ext := filepath.Ext(base)
-	fileDate := base[:len(base)-len(ext)]
-	if y, m, d := parseDate(fileDate); y == "" || m == "" || d == "" {
-		return fmt.Errorf("file not named YYYYMMDD.csv")
+// writeGnuplot writes a gnuplot file for plotting data from dataPath,
+// the path returned by an earlier writeData(ds) call.
+func writeGnuplot(ds *dataSet, dataPath string) (string, error) {
+	f, err := ioutil.TempFile("", "mortality.gnuplot.")
+	if err != nil {
+		return "", err
 	}
 
+	if err := template.Must(template.New("").Funcs(map[string]interface{}{
+		"indexCol": func(i int) int { return i + 2 },
+	}).Parse(`
+set title 'CDC Weekly Mortality for {{.State}}'
+
+set xlabel 'Reporting Date'
+set xdata time
+set timefmt '%Y%m%d'
+
+set ylabel 'Deaths'
+set yrange [0:*]
+
+set key autotitle columnheader bottom right title 'Week End'
+
+num_lines = {{.NumLines}}
+plot for [i=2:num_lines+2] '{{.DataPath}}' using 1:i with lines
+`)).Execute(f, struct {
+		State    string
+		DataPath string
+		NumLines int
+	}{ds.state, dataPath, len(ds.weekSeries)}); err != nil {
+		f.Close()
+		return "", err
+	}
+	return f.Name(), f.Close()
+}
+
+// timeseries contains the values of a variable at different points in time.
+type timeseries map[string]int
+
+// dataSet holds data for a state.
+// It parses CSV files downloaded on different days and tracks how each week's
+// reported mortality has changed over time.
+type dataSet struct {
+	state      string                // state name, e.g. "California"
+	start, end time.Time             // start and end dates
+	fileDates  map[string]struct{}   // dates of parsed data as e.g. "20200425"
+	weekSeries map[string]timeseries // keyed by week end as e.g. "20200425"
+}
+
+// newDataSet returns a new dataSet that saves mortality data for week-end
+// dates between start and end for the supplied state.
+func newDataSet(state string, start, end time.Time) *dataSet {
+	return &dataSet{
+		state:      state,
+		start:      start,
+		end:        end,
+		fileDates:  make(map[string]struct{}),
+		weekSeries: make(map[string]timeseries),
+	}
+}
+
+// readFile parses the CSV file at p.
+// The path's base filename must have the form 'YYYYMMDD.csv'
+// (describing the day when the file was downloaded).
+func (ds *dataSet) readFile(p string) error {
 	f, err := os.Open(p)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
+	// Extract the date from the filename.
+	base := filepath.Base(p)
+	ext := filepath.Ext(base)
+	fileDate := base[:len(base)-len(ext)]
+	if _, err := time.Parse(dateLayout, fileDate); err != nil {
+		return fmt.Errorf("file not named YYYYMMDD.csv: %v", err)
+	}
+	ds.fileDates[fileDate] = struct{}{}
+
 	r := csv.NewReader(f)
 
-	// Find the positions of various columns.
+	// Find the positions of columns that we care about.
 	cols, err := r.Read()
 	if err != nil {
 		return fmt.Errorf("failed reading header: %v", err)
@@ -83,11 +193,6 @@ func readFile(p, state, weekEnd string) error {
 		}
 	}
 
-	// The CDC changed the date format from MM/DD/YYYY to YYYY-MM-DD.
-	year, month, day := parseDate(weekEnd)
-	weekEnd1 := fmt.Sprintf("%s/%s/%s", month, day, year)
-	weekEnd2 := fmt.Sprintf("%s-%s-%s", year, month, day)
-
 	for {
 		vals, err := r.Read()
 		if err == io.EOF {
@@ -96,12 +201,87 @@ func readFile(p, state, weekEnd string) error {
 			return err
 		}
 
-		if vals[stateCol] == state &&
-			(vals[weekEndCol] == weekEnd1 || vals[weekEndCol] == weekEnd2) &&
-			vals[typeCol] == "Unweighted" && // vs. "Predicted (weighted)"
-			vals[outcomeCol] == "All causes" { // vs. "All causes, excluding COVID-19"
-			fmt.Printf("%s %s\n", fileDate, vals[observedCol])
+		if vals[stateCol] != ds.state ||
+			vals[typeCol] != "Unweighted" || // vs. "Predicted (weighted)"
+			vals[outcomeCol] != "All causes" { // vs. "All causes, excluding COVID-19"
+			continue
 		}
+
+		// The CDC started with dates formatted as MM/DD/YYYY but later changed to YYYY-MM-DD.
+		s := vals[weekEndCol]
+		weekEnd, err := time.Parse("2006-01-02", s)
+		if err != nil {
+			if weekEnd, err = time.Parse("01/02/2006", s); err != nil {
+				return fmt.Errorf("failed to parse week-end date %q: %v", s, err)
+			}
+		}
+		if weekEnd.Before(ds.start) || weekEnd.After(ds.end) {
+			continue
+		}
+
+		s = vals[observedCol]
+		if s == "" {
+			continue
+		}
+		observed, err := strconv.Atoi(s)
+		if err != nil {
+			return fmt.Errorf("failed to parse value %q: %v", s, err)
+		}
+
+		ws := weekEnd.Format(dateLayout)
+		ts, ok := ds.weekSeries[ws]
+		if !ok {
+			ts = make(timeseries)
+			ds.weekSeries[ws] = ts
+		}
+		ts[fileDate] = observed
 	}
 	return nil
+}
+
+// write writes ds's data to w in gnuplot's format, i.e. lines with tab-separated values.
+func (ds *dataSet) write(w io.Writer) error {
+	var writeErr error
+	write := func(s string) {
+		if writeErr == nil {
+			_, writeErr = io.WriteString(w, s)
+		}
+	}
+
+	weekEnds := make([]string, 0, len(ds.weekSeries))
+	for we := range ds.weekSeries {
+		weekEnds = append(weekEnds, we)
+	}
+	sort.Strings(weekEnds)
+
+	fileDates := make([]string, 0, len(ds.fileDates))
+	for fd := range ds.fileDates {
+		fileDates = append(fileDates, fd)
+	}
+	sort.Strings(fileDates)
+
+	// Put line names on the first line.
+	weekStrings := make([]string, len(weekEnds))
+	for i, we := range weekEnds {
+		t, _ := time.Parse(dateLayout, we)
+		weekStrings[i] = t.Format("01/02")
+	}
+	write("Date\t" + strings.Join(weekStrings, "\t") + "\n")
+
+	// Each following line starts with the reporting date from a file
+	// and the per-week data from the file.
+	for _, fd := range fileDates {
+		vals := make([]string, 0, 1+len(weekEnds))
+		vals = append(vals, fd)
+		for _, we := range weekEnds {
+			if v, ok := ds.weekSeries[we][fd]; ok {
+				vals = append(vals, strconv.Itoa(v))
+			} else {
+				vals = append(vals, "?")
+			}
+		}
+		write(strings.Join(vals, "\t") + "\n")
+	}
+
+	return writeErr
 }
