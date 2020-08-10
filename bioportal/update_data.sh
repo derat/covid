cc5241d386a179722fdb8a60057298a2801a5068
Daniel Erat <dan@erat.org>
2020-08-10 09:58:16 -0400

    bioportal: Delete update_data.sh.

--- bioportal/update_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-fn="minimal-info-unique-tests.$(date +%Y%m%d)"
-wget -O"$fn" \
-  https://bioportal.salud.gov.pr/api/administration/reports/minimal-info-unique-tests
-ln -sf "$fn" minimal-info-unique-tests
